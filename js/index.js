a24781a5f7b473b3cbc876be5a1a895bdb72c19e
zero1300 <2806374351z@gamil.com>
2019-05-24 12:24:44 +0800

    discover menu finish

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 var oUL = document.getElementById("list");
 
 
+// discover下拉菜单
+var mainMenu = document.getElementById("main-menu"); 
+var discover = document.getElementsByClassName("DISCOVER");
+var arrowBottom = document.getElementById("arrow-bottom");
+var show = 0;
+
+function showMenu(){
+    if (show === 0){
+        mainMenu.style.display = "inline";
+        show = 1;
+    }else{
+        mainMenu.style.display = "none";
+        show = 0;
+    }
+}
+discover[0].addEventListener("click", showMenu);
+arrowBottom.addEventListener("click", showMenu);
